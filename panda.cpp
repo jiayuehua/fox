035c06e013878af2c5b8971637b67a465a7e570e
jiayuehua <jiayuehua@le.com>
2016-12-09 15:23:56 +0800

    4 template algorithm

---
+++ panda.cpp
@@ -0,0 +1,38 @@
+#include <forward_list>
+template <class Container, class Iter>
+Iter eraseViaSwap(Container& c, Iter i)
+{
+  if (i== c.end())
+  {
+    return i;
+  }
+  auto e = c.end();
+  --e;
+  std::iter_swap(i, e);
+  c.pop_back();
+  return i;
+}
+
+template <class Container, class Iter>
+Iter erase(Container& c, Iter i)
+{
+  if (i== c.end())
+  {
+    return i;
+  }
+  *i = c.back();
+  c.pop_back();
+  return i;
+}
+
+template <class T>
+std::size_t size(const T & t)noexcept
+{
+  return t.size();
+}
+
+template <class U>
+std::size_t size(const std::forward_list<U> & fla)noexcept
+{
+  return std::distance(fla.begin(), fla.end());
+}
