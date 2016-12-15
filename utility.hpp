5608afc6a2c15af0fc03b628f3d7a55307e47664
jiayuehua <gracedancer@163.com>
2016-12-15 17:42:06 +0800

    string_view

---
+++ utility.hpp
@@ -0,0 +1,6 @@
+// use in string_view. 
+// Example:
+//    std::string_view s(SV_("hello,world"));
+// Compare to std::string_view s("hello,world"), there is no need to call strlen in runtime, instead compute "s" len in compile time
+// inspired by ngx_string
+#define SV_(a) a,sizeof(a)-1
