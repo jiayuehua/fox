b30b378123096239cb7f76358f5829fd3f4871f5
jiayuehua <gracedancer@163.com>
2016-12-15 17:46:53 +0800

    aes header file

---
+++ aes.hpp
@@ -0,0 +1,2 @@
+int base64Decode(unsigned char* In, int inLen, unsigned char* Out, int* pOutLen);
+int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key, unsigned char *plaintext);
