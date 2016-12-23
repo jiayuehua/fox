6e2107f7b8c1643e2de25051427f7dcc763e69f2
jiayuehua <gracedancer@163.com>
2016-12-23 18:09:06 +0800

    add condition compile macro

--- utility.hpp
+++ utility.hpp
@@ -3,4 +3,8 @@
 //    std::string_view s(SV_("hello,world"));
 // Compare to std::string_view s("hello,world"), there is no need to call strlen in runtime, instead compute "s" len in compile time
 // inspired by ngx_string
+#pragma once
+
+#ifndef SV_
 #define SV_(a) a,sizeof(a)-1
+#endif
