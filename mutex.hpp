28508d71479ec39dd8730a449de891b72b3fbe6a
jiayuehua <gracedancer@163.com>
2016-12-29 10:44:55 +0800

    Using boost parameter to make interface flexible

--- mutex.hpp
+++ mutex.hpp
@@ -1,42 +1,98 @@
 #pragma once
 #include <mutex>
-#include <shared_mutex>
+//#include <shared_mutex>
+#include <boost/parameter.hpp>
+#include <boost/mpl/arg.hpp>
+#include <boost/parameter/name.hpp>
+#include <boost/parameter/binding.hpp>
 namespace fox {
-  enum IsRecursive
+  using boost::mpl::_;
+  BOOST_PARAMETER_TEMPLATE_KEYWORD(RecursiveType)
+  BOOST_PARAMETER_TEMPLATE_KEYWORD(TimedType)
+  BOOST_PARAMETER_TEMPLATE_KEYWORD(SharedType)
+
+  class Shared
   {
-    NotRecursive,
-    Recursive,
   };
-  enum IsTimed
+  class NotShared:public Shared {
+
+  };
+  class CanShared:public Shared {
+
+  };
+  class Timed
   {
-    NotTimed,
-    Timed,
   };
-  enum IsShared {
-    NotShared,
-    Shared,
+  class NotTimed:public Timed {
+
   };
-  template<IsRecursive R = NotRecursive, IsTimed T = NotTimed, IsShared S = NotShared>
+  class CanTimed:public Timed {
+
+  };
+  class Rec
+  {
+  };
+  class NotRec:public Rec {
+
+  };
+  class CanRec :public Rec {
+
+  };
+  template<class T, class U, class V>
   class Mutex;
-
   template<>
-  class Mutex<NotRecursive, NotTimed, NotShared> : public std::mutex {
+  class Mutex<NotRec, NotTimed, NotShared> : public std::mutex {
   };
 
   template<>
-  class Mutex<Recursive, NotTimed, NotShared> : public std::recursive_mutex {
+  class Mutex<CanRec, NotTimed, NotShared> : public std::recursive_mutex {
   };
 
   template<>
-  class Mutex<NotRecursive, Timed, NotShared> : public std::timed_mutex {
+  class Mutex<NotRec, Timed, NotShared> : public std::timed_mutex {
   };
   template<>
-  class Mutex<Recursive, Timed, NotShared> : public std::recursive_timed_mutex {
+  class Mutex<CanRec, Timed, NotShared> : public std::recursive_timed_mutex {
   };
-  template<>
-  class Mutex<NotRecursive, NotTimed, Shared> : public std::shared_mutex {
+  //template<>
+  //class Mutex<NotRec, NotTimed, Shared> : public std::shared_mutex {
+  //};
+  //template<>
+  //class Mutex<NotRec, Timed, Shared> : public std::shared_timed_mutex {
+  //};
+  namespace parameter = boost::parameter;
+  typedef parameter::parameters<
+    parameter::optional<parameter::deduced<tag::RecursiveType>, boost::is_base_and_derived<Rec, _>>
+    , parameter::optional<parameter::deduced<tag::TimedType>, boost::is_base_and_derived<Timed, _>>
+    , parameter::optional<parameter::deduced<tag::SharedType>, boost::is_base_and_derived<Shared, _>>
+  > class_signature;
+  template <
+    class A0 = parameter::void_
+    , class A1 = parameter::void_
+    , class A2 = parameter::void_
+  >
+    struct MyMutexImpl
+  {
+    typedef typename
+      class_signature::bind<A0, A1, A2>::type
+      args;
+
+    // Extract first logical parameter.
+    typedef typename parameter::value_type<
+      args, tag::RecursiveType, NotRec >::type RecursiveType;
+
+    typedef typename parameter::value_type<
+      args, tag::TimedType, NotTimed >::type TimedType;
+
+    typedef typename parameter::value_type<
+      args, tag::SharedType, NotShared >::type SharedType;
+    typedef Mutex<RecursiveType, TimedType, SharedType> MutexType;
   };
-  template<>
-  class Mutex<NotRecursive, Timed, Shared> : public std::shared_timed_mutex {
-  };
+
+  template <
+    class A0 = parameter::void_
+    , class A1 = parameter::void_
+    , class A2 = parameter::void_
+  >
+    using GereralMutex = typename MyMutexImpl<A0, A1, A2>::MutexType;
 }
