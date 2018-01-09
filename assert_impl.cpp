4ca2ecb948a4444f19ef96aeef6a64424455152f
jiayuehua <jiayuehua@le.com>
2018-01-09 14:35:42 +0800

    assert impl

--- assert_impl.cpp
+++ assert_impl.cpp
@@ -4,6 +4,13 @@ namespace boost
 {
 void assertion_failed(char const* expr, char const* func, char const* file, long line)
 {
-  fprintf(stderr, "Assertion failed: %s, file %s, function %s, line %ld\n", expr, file, func, line);
+  fprintf(stderr, "Assertion failed: %s, file %s, function %s, line %ld\n",
+          expr, file, func, line);
+}
+void assertion_failed_msg(char const* expr, const char* msg, char const* func,
+                          char const* file, long line)
+{
+  fprintf(stderr, "Assertion failed: %s, message: %s, file %s, function %s, line %ld\n",
+          expr, msg, file, func, line);
 }
 }  // namespace boost
